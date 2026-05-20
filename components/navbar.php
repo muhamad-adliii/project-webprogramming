ae3a8f7a36eb86fd7fc7abe7afda2e65d5aeb0a3
unknown <choirunnisaauliaelfirdausi@gmail.com>
2026-05-20 15:37:56 +0700

    berita progrsss

--- components/navbar.php
+++ components/navbar.php
@@ -12,11 +12,11 @@
 <body>
     <nav>
         <div class="navbar-title">
-            <img src="/Project_website/images/logo hogwarts-jukebox-bg-removed.png" alt="">
+            <img src="/project-webprogramming/images/logo hogwarts-jukebox-bg-removed.png" alt="">
             <h3>hogwarts School</h3>
         </div>
         <ul>
-            <li><a href="/Project_website/index.php">Beranda</a></li>
+            <li><a href="/project-webprogramming/index.php">Beranda</a></li>
             <li class="profile-dropdown">
                 <div class="profile-icon">
                     <a href="">Profile</a>
@@ -27,8 +27,8 @@
                 </div>
 
                 <ul class="profile-list">
-                    <li><a href="/Project_website/pages/profile/visi-misi.php">Visi & Misi</a></li>
-                    <li><a href="/Project_website/pages/profile/ekstrakurikuler.php">Ekstrakurikuler</a></li>
+                    <li><a href="/project-webprogramming/pages/profile/visi-misi.php">Visi & Misi</a></li>
+                    <li><a href="/project-webprogramming/pages/profile/ekstrakurikuler.php">Ekstrakurikuler</a></li>
                 </ul>
             </li>
             <li class="keahlian-dropdown">
@@ -41,9 +41,10 @@
                 </div>
 
                 <ul class="keahlian-list">
-                    <li><a href="/Project_website/pages/jurusan/jurusan1.php">Rekayasa Perangkat Lunak</a></li>
-                    <li><a href="/Project_website/pages/jurusan/jurusan2.php">Teknik Komputer & Jaringan</a></li>
-                    <li><a href="/Project_website/pages/jurusan/jurusan3.php">Teknik Elektronika Industri</a></li>
+                    <li><a href="/project-webprogramming/pages/jurusan/jurusan1.php">Rekayasa Perangkat Lunak</a></li>
+                    <li><a href="/project-webprogramming/pages/jurusan/jurusan2.php">Teknik Komputer & Jaringan</a></li>
+                    <li><a href="/project-webprogramming/pages/jurusan/jurusan3.php">Teknik Elektronika Industri</a>
+                    </li>
                 </ul>
             </li>
             <li class="gallery-dropdown">
@@ -56,13 +57,13 @@
                 </div>
 
                 <ul class="gallery-list">
-                    <li><a href="/Project_website/pages/gallery/fasilitas.php">Fasilitas</a></li>
-                    <li><a href="/Project_website/pages/gallery/prestasi.php">Prestasi</a></li>
+                    <li><a href="/project-webprogramming/pages/gallery/fasilitas.php">Fasilitas</a></li>
+                    <li><a href="/project-webprogramming/pages/gallery/prestasi.php">Prestasi</a></li>
                 </ul>
             </li>
-            <li><a href="pages/berita.php">Berita</a></li>
-            <li><a href="pages/kontak.php">Kontak</a></li>
-            <li class="nav-daftar"><a href="/Project_website/pages/spmb.php">Pendaftaran</a></li>
+            <li><a href="/project-webprogramming/pages/berita.php">Berita</a></li>
+            <li><a href="/project-webprogramming/pages/kontak.php">Kontak</a></li>
+            <li class="nav-daftar"><a href="/project-webprogramming/pages/spmb.php">Pendaftaran</a></li>
         </ul>
     </nav>
 </body>
