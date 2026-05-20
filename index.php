682e7c69847969ee299b0ebba50a7deca9e66ecc
muhamad adli <muhmdadli01@gmail.com>
2026-05-21 02:12:50 +0700

    beranda progress

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/footer.css">
 </head>
 
 <body>
@@ -72,10 +73,13 @@
     </section>
 
     <section class="sambutan">
+        <div class="sambutan-img">
+            <img src="./images/bapa-removebg-preview.png" alt="">
+        </div>
         <div class="sambutan-desc">
             <div class="subtitle-container">
                 <span class="line"></span>
-                <span class="subtitle">Hogwarts School</span>
+                <span class="subtitle">Kepala Sekolah</span>
                 <span class="line"></span>
             </div>
             <div class="title">
@@ -84,29 +88,136 @@
             <div class="container-desc">
                 <p>Assalamualaikum wr wb. Salam sehat untuk kita semua. Selamat datang kami ucapkan kepada para
                     pengunjung yang senantiasa berada dalam lindungan Allah S.W.T di situs Hogwarts School.
-
+                    <br>
+                    <br>
                     Di situs Hogwarts School ini kami berusaha untuk dapat memberikan informasi-informasi yang
                     dibutuhkan, yaitu untuk siswa, guru, orang tua, maupun masyarakat. Terutama informasi yang
                     berhubungan dengan pendidikan, ilmu pengetahuan dan informasi tentang Hogwart School
-
+                    <br>
+                    <br>
                     Dalam melaksanakan proses pendidikan disekolah, kami selalu berusaha untuk dapat menyesuaikan dan
                     mengikuti perkembangan teknologi, yaitu teknologi informasi menuju revolusi industry 4.0
-
+                    <br>
+                    <br>
                     Dengan adanya media layanan informasi ini, kami harapkan dapat menambah wawasan, serta dapat
                     mempercepat dalam memberikan informasi-informasi yang dibutuhkan.
-
+                    <br>
+                    <br>
                     Terimakasih kami ucapkan kepada pengunjung situs ini, semoga dapat memberikan manfaat yang sangat
                     besar dan dapat berkunjung kembali
-
-                    Wassalamualaikum wr wb</p>
+                    <br>
+                    <br>
+                    Wassalamualaikum wr wb
+                </p>
             </div>
             <div class="container-footer">
                 <h4>KH. Albus Dumbledore, M.Pd.</h4>
                 <h4>Kepala Sekolah Hogwarts School</h4>
             </div>
         </div>
-
     </section>
+
+    <section class="information">
+        <div class="inf-card">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                <path fill="white"
+                    d="M320 80C377.4 80 424 126.6 424 184C424 241.4 377.4 288 320 288C262.6 288 216 241.4 216 184C216 126.6 262.6 80 320 80zM96 152C135.8 152 168 184.2 168 224C168 263.8 135.8 296 96 296C56.2 296 24 263.8 24 224C24 184.2 56.2 152 96 152zM0 480C0 409.3 57.3 352 128 352C140.8 352 153.2 353.9 164.9 357.4C132 394.2 112 442.8 112 496L112 512C112 523.4 114.4 534.2 118.7 544L32 544C14.3 544 0 529.7 0 512L0 480zM521.3 544C525.6 534.2 528 523.4 528 512L528 496C528 442.8 508 394.2 475.1 357.4C486.8 353.9 499.2 352 512 352C582.7 352 640 409.3 640 480L640 512C640 529.7 625.7 544 608 544L521.3 544zM472 224C472 184.2 504.2 152 544 152C583.8 152 616 184.2 616 224C616 263.8 583.8 296 544 296C504.2 296 472 263.8 472 224zM160 496C160 407.6 231.6 336 320 336C408.4 336 480 407.6 480 496L480 512C480 529.7 465.7 544 448 544L192 544C174.3 544 160 529.7 160 512L160 496z" />
+            </svg>
+            <p>peserta didik</p>
+            <span>2500</span>
+        </div>
+        <div class="inf-card">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                <path fill="white"
+                    d="M192 144C222.9 144 248 118.9 248 88C248 57.1 222.9 32 192 32C161.1 32 136 57.1 136 88C136 118.9 161.1 144 192 144zM176 576L176 416C176 407.2 183.2 400 192 400C200.8 400 208 407.2 208 416L208 576C208 593.7 222.3 608 240 608C257.7 608 272 593.7 272 576L272 240L400 240C417.7 240 432 225.7 432 208C432 190.3 417.7 176 400 176L384 176L384 128L576 128L576 320L384 320L384 288L320 288L320 336C320 362.5 341.5 384 368 384L592 384C618.5 384 640 362.5 640 336L640 112C640 85.5 618.5 64 592 64L368 64C341.5 64 320 85.5 320 112L320 176L197.3 176C151.7 176 108.8 197.6 81.7 234.2L14.3 324.9C3.8 339.1 6.7 359.1 20.9 369.7C35.1 380.3 55.1 377.3 65.7 363.1L112 300.7L112 576C112 593.7 126.3 608 144 608C161.7 608 176 593.7 176 576z" />
+            </svg>
+            <p>pengajar</p>
+            <span>100</span>
+        </div>
+        <div class="inf-card">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                <path fill="white"
+                    d="M320 312C253.7 312 200 258.3 200 192C200 125.7 253.7 72 320 72C386.3 72 440 125.7 440 192C440 258.3 386.3 312 320 312zM289.5 368L350.5 368C360.2 368 368 375.8 368 385.5C368 389.7 366.5 393.7 363.8 396.9L336.4 428.9L367.4 544L368 544L402.6 405.5C404.8 396.8 413.7 391.5 422.1 394.7C484 418.3 528 478.3 528 548.5C528 563.6 515.7 575.9 500.6 575.9L139.4 576C124.3 576 112 563.7 112 548.6C112 478.4 156 418.4 217.9 394.8C226.3 391.6 235.2 396.9 237.4 405.6L272 544.1L272.6 544.1L303.6 429L276.2 397C273.5 393.8 272 389.8 272 385.6C272 375.9 279.8 368.1 289.5 368.1z" />
+            </svg>
+            <p>tenaga pendidik</p>
+            <span>95</span>
+        </div>
+        <div class="inf-card">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+            </svg>
+            <p>kompetensi keahlian</p>
+            <span>3</span>
+        </div>
+    </section>
+
+    <section class="keahlian">
+        <div class="keahlian-title">
+            <h3>program keahlian</h3>
+        </div>
+        <div class="keahlian-container">
+            <div class="keahlian-card">
+
+                <h4>rekayasa perangkat lunak</h4>
+                <span>Rekayasa Perangkat Lunak atau biasa disingkat dengan RPL adalah salah satu bidang profesi dan juga
+                    mata pelajaran yang mempelajari tentang pengembangan Software Komputer termasuk dalam hal
+                    pembuatannya.</span>
+            </div>
+            <div class="keahlian-card">
+                <h4>teknik komputer dan jaringan</h4>
+                <span>Teknik Komputer dan Jaringan merupakan ilmu berbasis Teknologi Informasi dan Komunikasi terkait
+                    kemampuan algoritma, dan pemrograman komputer, perakitan komputer, perakitan jaringan komputer, dan
+                    pengoperasian perangkat lunak, dan internet.</span>
+            </div>
+            <div class="keahlian-card">
+                <h4>teknik elektronika industri</h4>
+                <span>Teknik Elektronika Industri merupakan kompetensi keahlian tingkat SMK yang berfokus pada peralatan
+                    pengendali mesin industri yang berbasiskan pengendali konvensional, Programmable Logic Control, dan
+                    mikrokontroler.</span>
+            </div>
+        </div>
+    </section>
+
+    <section class="berita">
+        <div class="berita-title">
+            <h4>Berita Terbaru</h4>
+        </div>
+        <div class="berita-container">
+            <div class="berita-card">
+                <img src="" alt="">
+                <h3>Rumah Kaca Hogwarts</h3>
+                <span>Suasana di dalam rumah kaca terasa sangat hidup dan detail. Cahaya matahari masuk melalui atap
+                    kaca yang berbentuk melengkung dengan rangka kayu yang kokoh. Di langit-langit, tergantung
+                    lampu-lampu gantung besar berbentuk unik yang memberikan kesan magis dan antik. Dinding di bagian
+                    belakang dipenuhi oleh tanaman merambat......</span>
+            </div>
+            <div class="berita-card">
+                <img src="" alt="">
+                <h3>Rumah Kaca Hogwarts</h3>
+                <span>Suasana di dalam rumah kaca terasa sangat hidup dan detail. Cahaya matahari masuk melalui atap
+                    kaca yang berbentuk melengkung dengan rangka kayu yang kokoh. Di langit-langit, tergantung
+                    lampu-lampu gantung besar berbentuk unik yang memberikan kesan magis dan antik. Dinding di bagian
+                    belakang dipenuhi oleh tanaman merambat......</span>
+            </div>
+            <div class="berita-card">
+                <img src="" alt="">
+                <h3>Rumah Kaca Hogwarts</h3>
+                <span>Suasana di dalam rumah kaca terasa sangat hidup dan detail. Cahaya matahari masuk melalui atap
+                    kaca yang berbentuk melengkung dengan rangka kayu yang kokoh. Di langit-langit, tergantung
+                    lampu-lampu gantung besar berbentuk unik yang memberikan kesan magis dan antik. Dinding di bagian
+                    belakang dipenuhi oleh tanaman merambat......</span>
+            </div>
+        </div>
+    </section>
+
+    <?php 
+    include "./components/footer.php"
+    ?>
 </body>
 
 </html>
