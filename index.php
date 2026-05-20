c63f3f2a11e69413b2a5ac2ec98d7e56abac7163
muhamad adli <muhmdadli01@gmail.com>
2026-05-20 20:53:08 +0700

    beranda progress

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         include 'components/navbar.php';
         ?>
         <div class="hero-card">
-            <div class="card-card 1">
+            <div class="card-card">
                 <div class="card-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
@@ -33,7 +33,7 @@
                     </div>
                 </div>
             </div>
-            <div class="card-card 2">
+            <div class="card-card">
                 <div class="card-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
@@ -49,28 +49,64 @@
                         dan berorientasi pada kebutuhan sihir masa depan.
                     </div>
                 </div>
-                <div class="card-card 3">
-                    <div class="card-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
-                        </svg>
-                        <div class="card-title">
-                            <h4>KURIKULUM BERBASIS SIHIR</h4>
-                        </div>
-                        <div class="card-desc">
-                            Hogwarts School menerapkan pembelajaran yang terhubung langsung dengan dunia sihir kreatif
-                            dan
-                            tongkat sihir. Melalui pendekatan proyek dan teaching factory, siswa belajar secara
-                            kontekstual
-                            dan berorientasi pada kebutuhan sihir masa depan.
-                        </div>
+            </div>
+            <div class="card-card">
+                <div class="card-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                    </svg>
+                    <div class="card-title">
+                        <h4>KURIKULUM BERBASIS SIHIR</h4>
+                    </div>
+                    <div class="card-desc">
+                        Hogwarts School menerapkan pembelajaran yang terhubung langsung dengan dunia sihir kreatif
+                        dan
+                        tongkat sihir. Melalui pendekatan proyek dan teaching factory, siswa belajar secara
+                        kontekstual
+                        dan berorientasi pada kebutuhan sihir masa depan.
                     </div>
                 </div>
+            </div>
     </section>
 
+    <section class="sambutan">
+        <div class="sambutan-desc">
+            <div class="subtitle-container">
+                <span class="line"></span>
+                <span class="subtitle">Hogwarts School</span>
+                <span class="line"></span>
+            </div>
+            <div class="title">
+                <h4>Sambutan</h4>
+            </div>
+            <div class="container-desc">
+                <p>Assalamualaikum wr wb. Salam sehat untuk kita semua. Selamat datang kami ucapkan kepada para
+                    pengunjung yang senantiasa berada dalam lindungan Allah S.W.T di situs Hogwarts School.
 
+                    Di situs Hogwarts School ini kami berusaha untuk dapat memberikan informasi-informasi yang
+                    dibutuhkan, yaitu untuk siswa, guru, orang tua, maupun masyarakat. Terutama informasi yang
+                    berhubungan dengan pendidikan, ilmu pengetahuan dan informasi tentang Hogwart School
+
+                    Dalam melaksanakan proses pendidikan disekolah, kami selalu berusaha untuk dapat menyesuaikan dan
+                    mengikuti perkembangan teknologi, yaitu teknologi informasi menuju revolusi industry 4.0
+
+                    Dengan adanya media layanan informasi ini, kami harapkan dapat menambah wawasan, serta dapat
+                    mempercepat dalam memberikan informasi-informasi yang dibutuhkan.
+
+                    Terimakasih kami ucapkan kepada pengunjung situs ini, semoga dapat memberikan manfaat yang sangat
+                    besar dan dapat berkunjung kembali
+
+                    Wassalamualaikum wr wb</p>
+            </div>
+            <div class="container-footer">
+                <h4>KH. Albus Dumbledore, M.Pd.</h4>
+                <h4>Kepala Sekolah Hogwarts School</h4>
+            </div>
+        </div>
+
+    </section>
 </body>
 
 </html>
